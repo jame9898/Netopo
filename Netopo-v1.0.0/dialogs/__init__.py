ffdb168b6ee88a87915f6c4b109b16251b3a01f1
jame9898 <82700237+jame9898@users.noreply.github.com>
2026-03-03 00:12:31 +0800

    Delete Netopo-v1.0.0 directory

--- Netopo-v1.0.0/dialogs/__init__.py
+++
@@ -1,7 +0,0 @@
-from .device_config_dialog import DeviceConfigDialog
-from .rack_device_dialog import RackDeviceDialog
-from .template_selection_dialog import TemplateSelectionDialog
-from .cable_tag_table_dialog import CableTagTableDialog
-from .device_name_dialog import DeviceNameDialog
-
-__all__ = ['DeviceConfigDialog', 'RackDeviceDialog', 'TemplateSelectionDialog', 'CableTagTableDialog', 'DeviceNameDialog']
--- Netopo-v1.0.0/rack/__init__.py
+++
@@ -1,6 +0,0 @@
-from .rack_device import RackDevice, RACK_WIDTH_PX, RACK_TOTAL_U, U_HEIGHT_PX, U_HEIGHT_OPTIONS
-from .rack_scene import RackScene
-from .u_slot import USlot
-
-__all__ = ['RackDevice', 'RackScene', 'USlot', 'RACK_WIDTH_PX', 'RACK_TOTAL_U', 
-           'U_HEIGHT_PX', 'U_HEIGHT_OPTIONS']
